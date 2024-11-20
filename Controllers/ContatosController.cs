0169cec558bd5f457d8224f8fb52da9d383dfd75
techub@799 <techub571@gmail.com>
2024-11-20 17:41:31 -0300

    feat: initial commit

---
+++ Controllers/ContatosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desapego.Controllers
+{
+    public class ContatosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PageContatos()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desapego.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PageProdutos()
+        {
+            return View();
+        }
+    }
+}
